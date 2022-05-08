2d451737528bee0fb535b6b2441ea8892aea074e
BeccaHurtado <rebeccah7.rh@gmail.com>
2022-05-07 17:55:41 -0700

    adding photos

---
+++ src/components/Nav/nav.js
@@ -0,0 +1,11 @@
+function Nav() {
+    return (
+        <div>
+            <a href="#about">Lorem ipsum dolor, sit amet consectetur adipisicing elit. Quo consequatur vitae voluptate nemo inventore modi aut non libero repellendus dolorem iusto, obcaecati molestias quos neque. Velit itaque quas harum asperiores.</a>
+        </div>
+    )
+} 
+
+export default Nav;
+
+
