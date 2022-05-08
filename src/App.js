bc74e52d98227a8142b597f3b8b348f5a7fe13e1
BeccaHurtado <rebeccah7.rh@gmail.com>
2022-05-07 18:40:24 -0700

    imported each componnent to app.js

--- src/App.js
+++ src/App.js
@@ -1,14 +1,24 @@
 import React from 'react'
 import Navigation from './components/Nav/nav'
-
+import { BrowserRouter, Switch, Route } from 'react-router-dom';
+import About from './components/About/about'
+import Portfolio from './components/Portfolio/portfolio'
+import Resume from './components/Resume/resume'
+import Contact from './components/Contact/contact'
 function App() {
-  return (
+  return (<BrowserRouter>
     <div className="App">
+      
       <Navigation></Navigation>
-      <main>
-      </main>
+      <Switch>
+      <Route exact path="/" component={About} />
+      <Route exact path="/portfolio" component={Portfolio} />
+      <Route exact path="/resume" component={Resume} />
+      <Route exact path="/contact" component={Contact} />
+      </Switch>
       <footer></footer>
     </div>
+    </BrowserRouter>
   );
 }
 
