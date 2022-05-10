222f39cd0e1b339cca8f051e418352fc9012c539
BeccaHurtado <rebeccah7.rh@gmail.com>
2022-05-10 14:08:52 -0700

    finishing about section

--- src/components/About/about.js
+++ src/components/About/about.js
@@ -8,79 +8,32 @@ function About() {
         <div className="header">
             <Container>
                 <Row>
-                    <Col className="fs-1 text-center pt-5">Rebecca Hurtado</Col>
+                    <Col className="text-center text-white fs-1">Rebecca Hurtado</Col>
                 </Row>
                 <Row>
                     <Col>
                         <img
                             src={portrait}
-                            className='img-fluid shadow-lg'
+                            className='img-fluid shadow-lg p-5'
                             alt=''
                             style={{ maxWidth: '24rem' }}
                         />
                     </Col>
                 </Row>
+                <Row>
+                    <Col>
+                    <i class="fa-brands fa-linkedin fa-2xl fa-bounce"></i>
+                    <i class="fa-solid fa-at fa-2xl fa-bounce"></i>
+                    <i class="fa-brands fa-github-alt fa-2xl fa-bounce"></i>
+                    </Col>
+                </Row>
+                <Row>
+                    <Col className="school pt-4">
+                    Full Stack Web Developer from Arizona at UofA. 
+                    </Col>
+                </Row>
             </Container>
         </div>
-
-
-
-
-        // <section className="container" id="about">
-        //     <Carousel>
-        //         <Carousel.Item>
-        //             <img
-        //                 className="d-block w-100"
-        //                 src={process.env.PUBLIC_URL + '/assets/aboutme/portrait.png'}
-        //                 alt="First slide"
-        //             />
-        //             <Carousel.Caption>
-        //                 <h3>First slide label</h3>
-        //                 <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
-        //             </Carousel.Caption>
-        //         </Carousel.Item>
-        //         <Carousel.Item>
-        //             <img
-        //                 className="d-block w-100"
-        //                 src={process.env.PUBLIC_URL + '/assets/aboutme/portrait.png'}
-        //                 alt="Second slide"
-        //             />
-
-        //             <Carousel.Caption>
-        //                 <h3>Second slide label</h3>
-        //                 <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
-        //             </Carousel.Caption>
-        //         </Carousel.Item>
-        //         <Carousel.Item>
-        //             <img
-        //                 className="d-block w-100"
-        //                 src={process.env.PUBLIC_URL + '/assets/aboutme/portrait.png'}
-        //                 alt="Third slide"
-        //             />
-
-        //             <Carousel.Caption>
-        //                 <h3>Third slide label</h3>
-        //                 <p>Praesent commodo cursus magna, vel scelerisque nisl consectetur.</p>
-        //             </Carousel.Caption>
-        //         </Carousel.Item>
-        //     </Carousel>
-        // </section>
-        // <Container>
-        //     <Row>
-        //         <Col xs={6} md={4} className="bg-image hover-overlay">
-        //             <Image src="/assets/aboutme/portrait.png" style={{ maxWidth: '16rem' }} />
-        //             <a href='#!'>
-        //                 <div className="mask overlay" style={{ backgroundColor: 'rgba(57, 192, 237, 0.2' }}></div>
-        //             </a>
-        //         </Col>
-        //         <Col xs={6} md={4}>
-        //             Rebecca Hurtado
-        //         </Col>
-        //         <Col xs={6} md={4}>
-        //             <Image src="/thumbnail.png" className="thumbnail" />
-        //         </Col>
-        //     </Row>
-        // </Container>
     )
 }
 
