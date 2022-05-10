0786072f04aad559a65afd15e6ba2f08c7485d50
BeccaHurtado <rebeccah7.rh@gmail.com>
2022-05-10 14:43:02 -0700

    adding all images to portfolio

--- src/components/Portfolio/portfolio.js
+++ src/components/Portfolio/portfolio.js
@@ -6,95 +6,74 @@ import javascript from './javascript-quiz.jpeg';
 import moveies from './moveies.png';
 import teamPortfolio from './team-portfolio.png'
 import weather from './weather-dashboard.png';
-import workday from './workday-scheduler.jpeg'
+import workday from './workday-scheduler.jpeg';
 
 function Portfolio() {
     return (
         <div className="main">
-            <Container className="pt-5">
-                <Row>
+            <Container>
+                <Row >
                     <Col>
-                        <Card>
-                            <Card.Img variant="top" src={moveies} />
-                            <Card.Body>
+                        <Card className="bg-dark text-white">
+                            <Card.Img src={javascript} alt="Card image" />
+                            <Card.ImgOverlay>
+                                <Card.Title className="text-dark">Card title</Card.Title>
                                 <Card.Text>
-                                    Some quick example text to build on the card title and make up the bulk
-                                    of the card's content.
+                                    This is a wider card with supporting text below as a natural lead-in to
+                                    additional content. This content is a little bit longer.
                                 </Card.Text>
-                            </Card.Body>
+                            </Card.ImgOverlay>
                         </Card>
                     </Col>
-
                     <Col>
-                        <Card>
-                            <Card.Img variant="top" src={dashboard} />
-                            <Card.Body>
-                                <Card.Text>
-                                    <div className="align-items-center">
-                                        <span><i class="fa-solid fa-arrow-left fa-fw" title="Back"></i></span>
-                                        <span><i class="fa-solid fa-arrow-right fa-fw" title="Forward"></i></span>
-                                        <span><i class="fa-solid fa-arrows-rotate fa-fw" title="Refresh"></i></span>
-                                        <span><i class="fa-solid fa-house fa-fw" title="Home"></i></span>
-                                        <span><i class="fa-solid fa-info fa-fw" title="Info"></i></span>
-                                        <span><i class="fa-solid fa-download fa-fw" title="Download"></i></span>
-                                    </div>
+                        <Card className="bg-dark text-white">
+                            <Card.Img src={dashboard} alt="Card image" />
+                            <Card.ImgOverlay>
+                                <Card.Title className="text-dark">Card title</Card.Title>
+                                <Card.Text className="text">
+                                    This is a wider card with supporting text below as a natural lead-in to
+                                    additional content. This content is a little bit longer.
                                 </Card.Text>
-                            </Card.Body>
+                            </Card.ImgOverlay>
                         </Card>
                     </Col>
-
                     <Col>
-                        <Card>
-                            <Card.Img variant="top" src={weather} />
-                            <Card.Body>
-                                <Card.Text>
-                                    <div className="align-items-center">
-                                        <span><i class="fa-solid fa-arrow-left fa-fw" title="Back"></i></span>
-                                        <span><i class="fa-solid fa-arrow-right fa-fw" title="Forward"></i></span>
-                                        <span><i class="fa-solid fa-arrows-rotate fa-fw" title="Refresh"></i></span>
-                                        <span><i class="fa-solid fa-house fa-fw" title="Home"></i></span>
-                                        <span><i class="fa-solid fa-info fa-fw" title="Info"></i></span>
-                                        <span><i class="fa-solid fa-download fa-fw" title="Download"></i></span>
-                                    </div>
-                                </Card.Text>
-                            </Card.Body>
+                        <Card className="bg-dark text-white">
+                            <Card.Img src={moveies} alt="Card image" className="moveies" />
                         </Card>
                     </Col>
                 </Row>
+            </Container>
 
-                <Row className="pt-4">
+            <Container>
+                <Row >
                     <Col>
-                        <Card>
-                            <Card.Img variant="top" src={javascript} />
-                            <Card.Body>
+                        <Card className="bg-dark text-white">
+                            <Card.Img src={teamPortfolio} alt="Card image" className="team" />
+                            <Card.ImgOverlay>
+                                <Card.Title className="text-dark">Card title</Card.Title>
                                 <Card.Text>
-                                    Some quick example text to build on the card title and make up the bulk
-                                    of the card's content.
+                                    This is a wider card with supporting text below as a natural lead-in to
+                                    additional content. This content is a little bit longer.
                                 </Card.Text>
-                            </Card.Body>
-                        </Card>
-                    </Col>
-
-                    <Col>
-                        <Card>
-                            <Card.Img variant="top" src={teamPortfolio} />
-                            <Card.Body>
-                                <Card.Text>
-                                    Some quick example text to build on the card title and make up the bulk
-                                    of the card's content.
-                                </Card.Text>
-                            </Card.Body>
+                            </Card.ImgOverlay>
                         </Card>
                     </Col>
                     <Col>
-                        <Card>
-                            <Card.Img variant="top" src={workday} />
-                            <Card.Body>
-                                <Card.Text>
-                                    Some quick example text to build on the card title and make up the bulk
-                                    of the card's content.
+                        <Card className="bg-dark text-white">
+                            <Card.Img src={weather} alt="Card image" className="weather"/>
+                            <Card.ImgOverlay>
+                                <Card.Title className="text-dark">Card title</Card.Title>
+                                <Card.Text className="text">
+                                    This is a wider card with supporting text below as a natural lead-in to
+                                    additional content. This content is a little bit longer.
                                 </Card.Text>
-                            </Card.Body>
+                            </Card.ImgOverlay>
+                        </Card>
+                    </Col>
+                    <Col>
+                        <Card className="bg-dark text-white">
+                            <Card.Img src={workday} alt="Card image" className="workday" />
                         </Card>
                     </Col>
                 </Row>
