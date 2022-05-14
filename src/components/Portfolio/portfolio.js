b5673f262b67335d6703fe3d41ad901ebfedc162
BeccaHurtado <rebeccah7.rh@gmail.com>
2022-05-14 15:54:09 -0700

    images showing with map feature

--- src/components/Portfolio/portfolio.js
+++ src/components/Portfolio/portfolio.js
@@ -3,47 +3,47 @@ import ProjectImages from '../../ProjectImg';
 
 
 function Portfolio() {
-    const { portfolioArray } = [
+    const portfolioArray = [
         {
             name: 'Dashboard',
-            github: '',
-            img: '../public/images/dashboard.png'
+            github: 'abd',
+            img: './images/dashboard.png'
         },
         {
             name: 'Dashboard',
             github: '',
-            img: '../public/images/dashboard.png'
+            img: './images/dashboard.png'
         },
         {
             name: 'Dashboard',
             github: '',
-            img: '../public/images/dashboard.png'
+            img: './images/dashboard.png'
         },
         {
             name: 'Dashboard',
             github: '',
-            img: '../public/images/dashboard.png'
+            img: './images/dashboard.png'
         },
         {
             name: 'Dashboard',
             github: '',
-            img: '../public/images/dashboard.png'
+            img: './images/dashboard.png'
         },
         {
             name: 'Dashboard',
             github: '',
-            img: '../public/images/dashboard.png'
+            img: './images/dashboard.png'
         },
     ]
     return (
         <section>
             <div>
             <h1 className="header">My Portfolio</h1>
-            </div>
-            <div className='conatiner'>
+            <div>
                 {portfolioArray.map(entry => {
                     return (<ProjectImages portfolioArray={entry}></ProjectImages>)
                 })}
+            </div>
             </div>
         </section>
     )
