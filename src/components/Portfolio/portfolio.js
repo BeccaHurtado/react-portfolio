e6f10b21cac791fc7276b53716e7b6964dbfa902
BeccaHurtado <rebeccah7.rh@gmail.com>
2022-05-20 20:20:43 -0700

    added github icon on images

--- src/components/Portfolio/portfolio.js
+++ src/components/Portfolio/portfolio.js
@@ -1,10 +1,11 @@
 // import React from "react";
 // import ProjectImages from '../../ProjectImg';
 import * as React from 'react';
+import GitHubIcon from '@mui/icons-material/GitHub';
 import ImageList from '@mui/material/ImageList';
 import ImageListItem from '@mui/material/ImageListItem';
 import ImageListItemBar from '@mui/material/ImageListItemBar';
-import { Typography, Container, Grid, Card, CardMedia, CardContent, CardActions, Button, Box } from '@mui/material'
+import { Grid, ListSubheader, IconButton} from '@mui/material'
 
 
 export default function About() {
@@ -49,29 +50,39 @@ export default function About() {
     ]
 
     return (
-        <Box xs={{ flexGrow: 1 }}>
-            <Grid container direction="column" alignItems="center" justifyContent="center">
-                <Grid item xs={1} sm={2} md={6} lg={12}>
-                    <ImageList sx={{ width: 900, height: 600 }} cols={2} gap={8}>
-                        {itemData.map((item) => (
-                            <ImageListItem>
-                                <img
-                                    src={`${item.img}?w=248&fit=crop&auto=format`}
-                                    srcSet={`${item.img}?w=248&fit=crop&auto=format&dpr=2 2x`}
-                                    alt={item.title}
-                                    loading="lazy"
-                                />
-                                <ImageListItemBar
-                                    title={item.title}
-                                    subtitle={<span>by: {item.author}</span>}
-                                    position="below"
-                                />
-                            </ImageListItem>
-                        ))}
-                    </ImageList>
-                </Grid>
-            </Grid>
-        </Box>
+    <div>
+    <Grid container direction="column" alignItems="center" justifyContent="center" wrap="wrap">
+    <Grid item >
+        <ImageList sx={{ width: 500, height: 450 }} sm={12} md={12} lg={12}>
+            <ImageListItem key="Subheader" cols={2}>
+                <ListSubheader component="div">December</ListSubheader>
+                </ImageListItem>
+                {itemData.map((item) => (
+                <ImageListItem key={item.img}>
+                    <img
+                    src={`${item.img}?w=248&fit=crop&auto=format`}
+                    srcSet={`${item.img}?w=248&fit=crop&auto=format&dpr=2 2x`}
+                    alt={item.title}
+                    loading="lazy"
+                    />
+                <ImageListItemBar
+                    title={item.title}
+                    subtitle={item.author}
+                    actionIcon={
+                    <IconButton
+                    x={{ color: 'rgba(255, 255, 255, 0.54)' }}
+                    aria-label={`info about ${item.title}`}
+                    >
+                    <GitHubIcon />
+                    </IconButton>
+                    }
+                    />
+            </ImageListItem>
+                ))}
+        </ImageList>
+    </Grid>
+    </Grid>
+    </div>
     );
 }
 
